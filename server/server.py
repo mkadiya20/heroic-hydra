399df5ddf67af8d5bde9c04dc015429262854f2b
Mihir Kadiya <mihir@Mihirs-MacBook-Pro.local>
2022-07-22 20:14:25 -0400

    updated server to support registration

--- server/server.py
+++ server/server.py
@@ -1,13 +1,42 @@
 from fastapi import FastAPI, WebSocket
+from submission import Submission
 
 app = FastAPI()
+submission = Submission()
+
+clients: dict[str, WebSocket] = {}
+
+
+async def register_user(username: str, websocket: WebSocket):
+    """Registers a user with the server if the username is not already taken"""
+    if username not in clients:
+        clients[username] = websocket
+        await websocket.send_text(f"Welcome! You are registered as {username}")
+    else:
+        await websocket.send_text("Username already taken")
+        await websocket.close()
+
+
+async def get_leaderboard():
+    """Returns the leaderboard"""
+    pass
 
 
 @app.websocket("/")
 async def root(websocket: WebSocket):
-    """This function is called when a websocket connection is made to http://localhost:8000/ (root of the api)."""
+    """This function is called when a websocket connection is made to http://localhost:8000/"""
     await websocket.accept()
 
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message received: {data}")
+        data = await websocket.receive_json()
+
+        if data["type"] == "register":
+            await register_user(data["username"], websocket)
+
+        elif data["type"] == "leaderboard":
+            await get_leaderboard()
+
+        elif data["type"] == "submission":
+            submission = Submission(data["data"])
+            # TODO: check if submission is valid
+            await submission.check_submission(data["data"])
