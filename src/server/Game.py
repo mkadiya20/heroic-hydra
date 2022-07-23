90bf04fd5f8f501b148f8be67262be43c124fc3f
Cyrus Kirk <cyrusjkirk@gmail.com>
2022-07-23 17:37:58 -0400

    update game and user

--- src/server/Game.py
+++ src/server/Game.py
@@ -1,3 +1,9 @@
+from error_thrower import Error_Objective
+from user import User
+
+error_obj = Error_Objective()
+
+
 class UserError(Exception):
     """Is used to represent errors with users."""
 
@@ -6,40 +12,50 @@ class Game:
     """Represents a game, a instantiate this class to get the game started."""
 
     def __init__(self) -> None:
-        self.game = {}
+        self.users = {}
         self.users_submitted = 0
 
-    async def login(self, user: str) -> None:
+    async def login(self, username: str) -> None:
         """Adds a user to the game."""
-        if user not in self.game.keys():
-            self.game[user] = 0
+        if username not in self.users.keys():
+            self.users[username] = User(username)
         else:
             raise UserError("User already exists.")
 
-    async def logout(self, user: str) -> None:
-        """Removes a user to the game."""
-        if user in self.game.keys():
-            del self.game[user]
+    async def logout(self, username: str) -> None:
+        """Removes a user from the game."""
+        if username in self.users.keys():
+            del self.users[username]
         else:
-            raise UserError("User already does not exist.")
-
-    async def add(self, user: str, points: int) -> None:
-        """Adds points to a user."""
-        try:
-            self.game[user] += points
-        except KeyError:
             raise UserError("User does not exist.")
 
-    async def remove(self, user: str, points: int) -> None:
-        """Deducts points from a user."""
-        try:
-            self.game[user] -= abs(points)
-        except KeyError:
+    async def submit(self, username: str, submission_str: str) -> None:
+        """Evaluates the user's submission and changes their score"""
+        if username in self.users.keys():
+            self.users[username].submit(submission_str)
+        else:
             raise UserError("User does not exist.")
 
-    async def user(self, user: str):
+    async def user_score(self, username: str):
         """Returns the points for a user."""
         try:
-            return self.game[user]
+            return self.users[username].score
         except KeyError:
             raise UserError("User does not exist.")
+
+    async def new_target(self, username: str):
+        """Updates the user's target error"""
+        try:
+            new_error = error_obj.objective()
+            self.users[username].assign_objective(new_error)
+        except KeyError:
+            raise UserError("User does not exist.")
+
+    async def get_leaderboard(self):
+        """Returns the leaderboard"""
+        string = ""
+        c = 1
+        for k, v in self.users.items():
+            string += "{c}. {k} - {v.points} \n"
+            c += 1
+        return string
