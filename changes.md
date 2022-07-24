06b7cde58083967b386e9bcf917b7569fd208084
PlurallyDialLLC <dial@plurallydial.com>
2022-07-24 17:19:36 -0400

    Error Class V2.2
    Signed-off-by: PlurallyDialLLC <dial@plurallydial.com>

--- changes.md
+++ changes.md
@@ -19,3 +19,29 @@
 > > > errors = Error_Objective()
 > > > errors.objective(difficulty=...)  # EASY | MOD | HARD
 > > > ```
+> > > 
+> > > 
+# Update 07/24/2022 5:19 PM EST
+### REMOVAL OF "toggle_difficulty_level"
+> ``toggle_difficulty_level`` has been removed.
+
+### CHANGES TO "objective"
+> ``objective`` now requres you to provide a ``difficulty`` parameter when called.
+> 
+> ``objective`` no longer takes ``amount`` as a parameter.
+> 
+> ``objective`` will raise an exception if difficulty is not provided.
+> 
+> ``objective`` will raise ``DifficultyObjectivesCompleted`` if ``already_used_keywords`` is equal to ``self.ERRORS[difficulty]``
+> 
+> ``objective`` now allows you to provide ``already_used_keywords`` as a parameter, to prevent duplicate objectives.
+> > Old Usage:
+> > > ```python
+> > > errors = Error_Objective()
+> > > errors.objective(amount=...)
+> > > ```
+> > New Usage:
+> > > ```python
+> > > errors = Error_Objective()
+> > > errors.objective(difficulty=..., already_used_keywords= list | None)  # EASY | MOD | HARD
+> > > ```
--- error_thrower.py
+++ error_thrower.py
@@ -29,73 +29,118 @@
 #  ValueError,
 #  ZeroDivisionError
 
+"""Copyright 2021 Python Discord
 
-# REQURE
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 
 import random
-from termcolor import colored
+
+
+class DifficultyObjectivesCompleted(Exception):
+    """Raised when all objectives in self.ERROR[difficulty] is exactly equal to already_used_keywords."""
+    __module__ = 'builtins'
+    pass
+
+
+class AllObjectivesCompleted(Exception):
+    """Raised when all objectives in self.ERROR is exactly equal to already_used_keywords."""
+    __module__ = 'builtins'
+    raise NotImplementedError  # Currently Unused
 
 
 class Error_Objective:
+    """Master Class for Error Objective Calling."""
+
     def __init__(self):
         # TO-DO ( Dashed to allow commit)
         # Add more Exceptions.
         # Optimize Order
         # - Dial, 07/23/2022 9:32 PM EST
-        self.ERRORS = {"EASY": {"KeyError",
-                                "AssertionError",
-                                "ValueError",
-                                "ZeroDivisionError",
-                                "KeyboardInterrupt",
-                                "IndentationError",
+        self.UNUSED_ERRORS = {"NotImplementedError",
+                              "PendingDeprecationWarning",
+                              "FloatingPointError",
+                              "StopAsyncIteration",
+                              "Exception",
+                              "BufferError",
+                              "LookupError",
+                              "BaseException"
+                              }  # UNUSED / BASECLASS / REQUIRE "RAISE" https://docs.python.org/3/library/exceptions.html#built-in-exceptions
+
+        self.ERRORS = {"EASY": {"IndexError",
+                                "KeyError",
                                 "TypeError",
-                                "ImportError",
                                 "AttributeError",
-                                "SystemExit",
-                                "IndexError",
+                                "ZeroDivisionError",
+                                "ValueError",
+                                "AssertionError",
+                                "OverflowError",
+                                "OSError",
+                                "NameError",
+                                "IndentationError",
                                 "TabError",
-                                "ReferenceError",
-                                "NotADirectoryError",
-                                "FileNotFoundError"
+                                "IOError",
+                                "ImportError",
+                                "KeyboardInterrupt",
+                                "SyntaxError",
+                                "SystemExit",
+                                "UnboundLocalError",
+                                "LookupError",
+                                "EOFError",
+                                "RecursionError"
                                 },  # "Easy" Exceptions.
 
-                       "MOD": {"MemoryError",
-                               "GeneratorExit",
-                               "OverflowError",
-                               "EOFError",
-                               "StopIteration"
+                       "MOD": {"GeneratorExit",
+                               "StopIteration",
+                               "MemoryError",
+                               "StopIteration",
+                               "UnicodeError",
+                               "UnicodeEncodeError",
+                               "UnicodeDecodeError",
+                               "DeprecationWarning"
                                },  # "Moderate" Exceptions
 
-                       "HARD": {"UnboundLocalError",
-                                "UnicodeError",
-                                "UnicodeTranslateError",
-                                "UnicodeEncodeError",
-                                "UnicodeDecodeError",
-                                "SystemError",
-                                "OSError",
-                                "IOError",
-                                "NotImplementedError",
-                                "NotImplemented",
-                                "DeprecationWarning"
-                                }
+                       "HARD": {"SystemError",
+                                "EnvironmentError",
+                                "RuntimeError",
+                                "BufferError"
+                                }  # "Hard" Exceptions
 
                        }
-    def toggle_difficulty_level(self, *args):
-        raise DeprecationWarning("toggle_difficulty_level has been removed, please use 'objective(difficulty=...)' ")
 
-    def objective(self, difficulty: str = None, amount: int = 1):
+    def objective(self, difficulty: str, already_used_keywords: list | tuple = None):
         try:
-            if difficulty is None:
-                raise SyntaxError("Difficulty is required due to toggle_difficulty_level being removed.")
-
-            if amount > 1:
-                raise DeprecationWarning("This is being removed; objective() will only return one exception per call.")
-            else:
+            if already_used_keywords is None:
                 return list(self.ERRORS[difficulty])[random.randint(0, len(self.ERRORS[difficulty]) - 1)]
+            else:
+                for x in self.ERRORS[difficulty]:
+                    choice = list(self.ERRORS[difficulty])[random.randint(0, len(self.ERRORS[difficulty]) - 1)]
+                    if choice not in already_used_keywords:
+                        return choice
+                    else:
+                        continue
+                counter = 0
+                for x in self.ERRORS[difficulty]:
+                    for y in already_used_keywords:
+                        if x == y:
+                            counter += 1
+                        else:
+                            continue
+                if counter == len(self.ERRORS[difficulty]):
+                    raise DifficultyObjectivesCompleted(f"All Objectives in {difficulty} are completed.")
+
+        except DifficultyObjectivesCompleted:
+            raise DifficultyObjectivesCompleted(f"All Objectives in {difficulty} are completed.")
+
         except Exception as err:
             raise Exception(
                 f"Something went wrong in '{str(__name__)[2:len(__name__) - 2]},' the Exception in question is {str(type(err))[7:len(str(type(err))) - 1]}. \n The error is as follows: \n {str(err)}")
 
 
 errors = Error_Objective()
-print(errors.objective(difficulty=...)) # EASY | MOD | HARD
+errors.objective(difficulty=...,
+                 already_used_keywords=...)  # difficulty = "EASY | MOD | HARD", already_used_keywords = list or None
