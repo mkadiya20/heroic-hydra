47e6b7ee0b95f9417d44cfb26093218630149d00
PlurallyDialLLC <dial@plurallydial.com>
2022-07-27 16:50:58 -0400

    Point Difficulty.
    Signed-off-by: PlurallyDialLLC <dial@plurallydial.com>

--- src/server/point_hardness.py
+++ src/server/point_hardness.py
@@ -1,72 +1,45 @@
-from user import User, MOD_THRESH, HARD_THRESH
+import asyncio
 
-"""
-        888                    
-        888                    
-        888                    
-.d8888b 888888 .d88b. 88888b.  
-88K     888   d88""88b888 "88b 
-"Y8888b.888   888  888888  888 
-     X88Y88b. Y88..88P888 d88P 
- 88888P' "Y888 "Y88P" 88888P"  
-                      888      
-                      888      
-                      888      
-                      
-Don't use yet. Not Finished nor tested.
-- Dial
+from user import User
+from error_thrower import Error_Objective
 
 
-"""
-
-
-
-class PointManagerError(Exception):
-    """Raised when an exception occurs in PointManager"""
+class PointHandlerError(Exception):
+    """Raised when an exception occurs inside PointHandler"""
     __module__ = "builtins"
 
 
-class PointMathError(Exception):
-    """Raised when a math function fails."""
-    __module__ = "PointManagerError"
+class PointHandler:
 
-
-class PointManager:
-
-    @staticmethod
-    async def solve_time_score(local_user: User, solve_time: int):
+    async def point(self, assignment: str, client_user: User):
         try:
-            match solve_time:
-                case range(0, 2):
-                    local_user.score += 3
-                case range(3, 4):
-                    local_user.score += 2
+            match await self.objective_checker(assignment):
+                case 1:
+                    client_user.score += 1
+                case 2:
+                    client_user.score += 1
+                case 3:
+                    client_user.score += 1
                 case _:
-                    local_user.score += 1
-        except BaseException as err:
-            raise PointMathError(
-                f"Type: {str(type(err))[7:len(str(type(err))) - 1]} \n \n Cause: {err.__cause__} \n \n Message: {err}")
-        return 0
+                    raise ValueError("Difficulty must be an int with the range of 1 to 3.")
+            return client_user.score
+        except BaseException as exception:
+            raise PointHandlerError(
+                f" \n Type: {str(type(exception))[7:len(str(type(exception))) - 1]} \n \n Reason: {exception}")
 
-    @staticmethod
-    async def account_for_difficulty(local_user: User):
+    async def objective_checker(self, assignment: str, error_obj: Error_Objective = Error_Objective()):
         try:
-            if HARD_THRESH <= local_user.score:
-                local_user.score += 3
-            elif HARD_THRESH > local_user.score >= MOD_THRESH:
-                local_user.score += 2
-            else:
-                local_user.score += 1
-        except BaseException as err:
-            raise PointMathError(
-                f"Type: {str(type(err))[7:len(str(type(err))) - 1]} \n \n Cause: {err.__cause__} \n \n Message: {err}")
-        return 0
+            for x in error_obj.ERRORS:
+                for y in error_obj.ERRORS[x]:
+                    if assignment == y:
+                        return x
+                    else:
+                        continue
+            raise PointHandlerError(f"{assignment} not found")
+        except BaseException as exception:
+            raise PointHandlerError(
+                f" \n Type: {str(type(exception))[7:len(str(type(exception))) - 1]} \n \n Reason: {exception}")
 
-    async def point(self, local_user: User, solve_time_seconds: int):
-        try:
-            await self.account_for_difficulty(local_user)
-            await self.solve_time_score(local_user, solve_time_seconds)
-        except BaseException as err:
-            raise PointManagerError(
-                f"Type: {str(type(err))[7:len(str(type(err))) - 1]} \n \n Cause: {err.__cause__} \n \n Message: {err}")
 
+pointhandle = PointHandler()
+foo = asyncio.run(pointhandle.point("ValueError", User("Bob")))
