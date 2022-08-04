eddb46ef8c6ab450a8b79595598b335b2b182347
HRLO77 <shakebmohammad.10@gmail.com>
2022-08-03 20:28:39 -0400

    changed console client

--- src/server/test_client.py
+++ src/server/test_client.py
@@ -13,18 +13,27 @@ async def handle(websocket, resp: dict):
 
     if resp["type"] == "objective":
         print(resp["data"])
+        return
 
     if resp["type"] == "login":
         print(resp["data"])
+        return
 
     if resp["type"] == "submit":
         print(resp["data"])
+        await websocket.recv()
+        return
 
     if resp["type"] == "leaderboard":
         c = 1
         for k, v in resp["data"].items():
             print(f"{c}. {k} - {v}")
             c += 1
+        return
+            
+    if resp['type'] == 'login':
+        print(resp['data'])
+        return
 
     else:
         print(f"Unsupported response type - {resp['type']}")
@@ -37,6 +46,7 @@ async def main():
         user = input("Enter username: ")
         await socket.send(json.dumps({"type": "register", "data": user}))
         await handle(socket, await socket.recv())
+        await socket.recv()
         while socket.open:
             await handle(socket, await socket.recv())
             inputted = input(":")
